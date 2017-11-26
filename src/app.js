d1b9a9e14c7803287bd91165cda57f9f6d15123f
Stefan Wapnick <stefanwapnick@hotmail.com>
2017-11-26 17:42:28 -0500

    Added books router

--- src/app.js
+++ src/app.js
@@ -4,6 +4,15 @@ var app = express();
 // Note that nodemon will set PORT variable when it runs in gulp
 var port = process.env.PORT || 3000;
 
+var bookRouter = express.Router();
+
+bookRouter.route('/books')
+    .get(function(req, res){
+        res.json({hello: 'value'});
+    });
+
+app.use('/api', bookRouter);
+
 app.get('/', function(req, res){
     res.send('Welcome to my API');
 });
